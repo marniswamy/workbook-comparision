603cc21f15730ec45fd956f961eed8a2bbc5330c
Marni Ayyappa Swamy <ayyappa.marni@hotmail.com>
2022-06-03 22:12:49 +0200

    csv uploader and parser added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
+import Papa from "papaparse";
 
-function App() {
+export const App = () => {
+  const [headerRow, setHeaderRow] = useState([]);
+  const [dataRows, setDataRows] = useState([]);
+
+  const handleOnChange = (event) => {
+    Papa.parse(event.target.files[0], {
+      header: true,
+      skipEmptyLines: true,
+      complete: function (results) {
+        console.log(results.data);
+        const headerData = [];
+        const tableData = [];
+
+        results.data.map((item) => {
+          headerData.push(Object.keys(item));
+          tableData.push(Object.values(item));
+        });
+
+        setHeaderRow(headerData[0]);
+        setDataRows(tableData);
+      },
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <h1>File comparison and formatting</h1>
+      <div className="app-container">
+        <div>
+          <input
+            type="file"
+            name="file"
+            accept=".csv"
+            placeholder="Please choose CSV file"
+            onChange={handleOnChange}
+          />
+          <div>
+            {!!headerRow.length && (
+              <table>
+                <thead>
+                  <tr>
+                    {headerRow.map((item, index) => (
+                      <th key={index}>{item}</th>
+                    ))}
+                  </tr>
+                </thead>
+                <tbody>
+                  {dataRows.map((value, index) => {
+                    return (
+                      <tr key={index}>
+                        {value.map((val, i) => {
+                          return <td key={i}>{val}</td>;
+                        })}
+                      </tr>
+                    );
+                  })}
+                </tbody>
+              </table>
+            )}
+          </div>
+        </div>
+        <div>Section 2</div>
+      </div>
     </div>
   );
-}
-
-export default App;
+};
