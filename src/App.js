ef154e28562e9a1c7a1836f97e8aa08862fa4574
Marni Ayyappa Swamy <ayyappa.marni@hotmail.com>
2022-06-05 17:14:42 +0200

    file comparison message added

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import dataToJson from "data-to-json";
 export const App = () => {
   const [csvData, setCsvData] = useState([]);
   const [prnData, setPrnData] = useState([]);
+  const [message, setMessage] = useState(null);
 
   const handleFileSelect = ({ target }) => {
     const file = target.files[0];
@@ -36,12 +37,32 @@ export const App = () => {
     }
   };
 
+  const handleCompare = () => {
+    const parseCsvData = JSON.stringify(csvData);
+    const parsePrnData = JSON.stringify(prnData);
+
+    if (parseCsvData === parsePrnData) {
+      return setMessage("Both the file data is same");
+    }
+    return setMessage("Both the files are different");
+  };
+
   return (
     <div className="wrapper">
       <h1>File comparison and formatting</h1>
       <div className="compare-text">
-        <button>Compare both files</button>
-        <p>Both the files are same</p>
+        <button
+          className="compare-button"
+          disabled={!csvData.length || !prnData.length}
+          onClick={handleCompare}
+        >
+          Compare both files
+        </button>
+        {message && (
+          <div className="message">
+            {message} <span onClick={() => setMessage(null)}>X</span>
+          </div>
+        )}
       </div>
       <div className="app-container">
         <div>
